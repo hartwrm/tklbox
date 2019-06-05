7a7991fee54224f79f5c44634f3c848129398716
Andrew Hartzell <hartwrm@Macs-MacBook-Pro.local>
2019-06-04 22:25:49 -0400

    new.ejs

--- controllers/lures.js
+++ controllers/lures.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router()
 const Lure = require('../models/lures.js')
 
+
+//INDEX
 router.get('/', (req, res) => {
   res.render('index.ejs')
 })
+//NEW
+router.get('/new', (req, res) => {
+  res.render('new.ejs')
+})
 
+//CREATE
+router.post('/', (req, res) => {
+  
+})
 
 module.exports = router;
