2eb1106a67f58f283787cd8558ff8053f7210f48
Andrew Hartzell <hartwrm@Macs-MacBook-Pro.local>
2019-06-05 19:41:34 -0400

    plastics page working

--- models/lures.js
+++ models/lures.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const lureSchema = new Schema ({
-  type: String,
+  type: {type: String, enum:['Plastic', 'Top water', 'Crank Bait', 'Jerkbait', 'Swim Bait', 'Jig']},
   name: String,
-  cond: [String],
+  conditions: [String],
   img: String,
   qty: {type: Number, min: 0}
 }, {timestamps: true})
